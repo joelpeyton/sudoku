b10b820a0d23eb2530f91eebe789a5e21dca8750
Joel Peyton <joelpeyton@hotmail.co.uk>
2022-12-01 13:38:22 +0000

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <div className="fixed-top p-1">
+                <a href="https://joelpeyton.github.io/Portfolio/">
+                    <i className="bi bi-github p-1"></i>
+                </a> 
+            </div>
+            <h3 className="m-5">Sudoku</h3>
+        </>
+    );
+}
+
+export default Header;

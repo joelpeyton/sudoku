601e002ef8e554ad8ba888a7eb304b145c25bdca
Joel Peyton <joelpeyton@hotmail.co.uk>
2022-12-09 22:14:41 +0000

    additional tweak

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,11 +1,9 @@
 function Github() {
     return (
         <div className="row">
-            <div className="col mb-5">
-                <a href="https://github.com/joelpeyton">
-                    <i className="bi bi-github p-1 me-5" style={{float: "right"}}></i>
-                </a> 
-            </div>
+            <a href="https://github.com/joelpeyton">
+                <i className="bi bi-github p-1" style={{float: "right"}}></i>
+            </a> 
         </div>
     );
 }
